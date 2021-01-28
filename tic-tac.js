d4a3e760a62432ecc78f0d21a11bc831ad5ba175
Peterturn <55192090+Peterturn@users.noreply.github.com>
2021-01-28 14:33:55 +0100

    added starting logic and notes in the JavaScript file

--- tic-tac.js
+++ tic-tac.js
@@ -1,11 +1,18 @@
 const tdClickArea = document.querySelectorAll("td");
 
 
-
-
+/** 
+ * Event Listener Logic. Clicks based on turn will return an "X" or "O".
+ * if 3 are in a row veritcal, diagonal, or horizontal the player wins.
+ * else game is a tie.
+ * 
+ */
 tdClickArea.forEach(item => {
     item.addEventListener('click', e => {
         let btnArea = e.target;
-        console.log(btnArea);
+
+        // Replaces inner HTML of the Cell
+        btnArea.innerHTML = "B";
+
     })
 });
