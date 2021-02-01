eb7b801e448bafb98046af754d7999e8889564d1
Peterturn <55192090+Peterturn@users.noreply.github.com>
2021-02-01 09:38:20 +0100

    added new logic for win condition and replaced regex with boolean array

--- tic-tac.js
+++ tic-tac.js
@@ -3,22 +3,8 @@ const playerTurnMsg = document.getElementById('player-turn');
 const resultMessage = document.getElementById("root");
 const newGameBtn = document.getElementById("new-game");
 newGameBtn.disabled = true;
-//Test Arrays
 
-let joueur1Sq = [];
-let joueur2Sq = [];
 let counter = 0;
-
-let win = [
-    /(?:^|\W)[0-2][0-2][0-2](?:$|\W)/gm,
-    /(?:^|\W)[3-5][3-5][3-5](?:$|\W)/gm,
-    /(?:^|\W)[6-8][6-8][6-8](?:$|\W)/gm,
-    /(?:^|\W)[036][036][036](?:$|\W)/gm,
-    /(?:^|\W)[147][147][147](?:$|\W)/gm,
-    /(?:^|\W)[258][258][258](?:$|\W)/gm,
-    /(?:^|\W)[048][048][048](?:$|\W)/gm,
-    /(?:^|\W)[246][246][246](?:$|\W)/gm,
-]
 
 
 //Game vars
@@ -26,7 +12,36 @@ let joueur1Turn = true;
 let joueur2Turn = false;
 let joueur1Win = false;
 let joueur2Win = false;
+
+
 playerTurnMsg.className = "joueur1"
+
+
+//Test Arrays
+let joueur1Sq = [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+
+];
+let joueur2Sq = [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+];
+
 
 function changeTurn() {
     if (joueur1Turn) {
@@ -42,17 +57,36 @@ function changeTurn() {
     }
 }
 
+function winGame(array) {
+    //Horizontal Wins
+    if (array[0] == true && array[1] == true && array[2] == true) {
+        alert("great job you won")
+    } else if (array[3] == true && array[4] == true && array[5] == true) {
+        alert("great job you won")
+    } else if (array[6] == true && array[7] == true && array[8] == true) {
+        alert("great job you won")
+    }
+    //Vertical Wins
+    else if (array[0] == true && array[3] == true && array[6] == true) {
+        alert("great job you won")
+    } else if (array[1] == true && array[4] == true && array[7] == true) {
+        alert("great job you won")
+    } else if (array[2] == true && array[5] == true && array[8] == true) {
+        alert("great job you won")
+    }
+    //Diagonal Wins
+    else if (array[2] == true && array[4] == true && array[6] == true) {
+        alert("great job you won")
+    } else if (array[0] == true && array[4] == true && array[8] == true) {
+        alert("great job you won")
+    }
+}
 
-function endGameEvaluation(counter) {
-    if (joueur1Win) {
-        resultMessage.innerHTML = `<div id="resultMessage">Player 1 Wins!! End of the game</div>`;
-        newGameBtn.disabled = false;
-    } else if (joueur2Win) {
-        resultMessage.innerHTML = `<div id="resultMessage">Player 2 Wins!! End of the game</div>`;
-        newGameBtn.disabled = false;
-    } else if (counter == 9) {
+function endGameEvaluation() {
+    if (counter == 9) {
         resultMessage.innerHTML = `<div id="resultMessage">End of the game</div>`;
         newGameBtn.disabled = false;
+        playerTurnMsg.innerHTML = "";
     }
 }
 
@@ -60,8 +94,28 @@ function resetGame() {
     tdClickArea.forEach(item => {
         item.innerHTML = "";
         counter = 0;
-        joueur1Sq = [];
-        joueur2Sq = [];
+        joueur1Sq = [
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+        ];
+        joueur2Sq = [
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+        ];
         joueur1Win = false;
         joueur2Win = false;
         changeTurn();
@@ -73,44 +127,23 @@ function resetGame() {
 tdClickArea.forEach(item => {
     item.addEventListener('click', e => {
         let btnArea = e.target;
-        let tempArray1 = [];
-        let tempArray2 = [];
 
         if (btnArea.innerHTML == "X" || btnArea.innerHTML == "O") {
             alert("tricheur! Choisi une autre case")
         } else {
             if (joueur1Turn) {
                 btnArea.innerHTML = "X";
-                joueur1Sq.push(btnArea.getAttribute("value"));
-                tempArray1 = joueur1Sq.join("");
+                joueur1Sq[btnArea.getAttribute("value")] = true;
+                winGame(joueur1Sq);
+
             } else if (joueur2Turn) {
                 btnArea.innerHTML = "O";
-                joueur2Sq.push(btnArea.getAttribute("value"));
-                tempArray2 = joueur2Sq.join("");
+                joueur2Sq[btnArea.getAttribute("value")] = true;
+                winGame(joueur2Sq)
             }
             counter++;
             changeTurn();
             endGameEvaluation(counter);
-        }
-        //
-        if (tempArray1.length >= 3) {
-            for (let i = 0; i < 8; i++)
-                if (tempArray1.match(win[i])) {
-                    counter = 9;
-                    joueur1Win = true;
-                    endGameEvaluation(counter);
-                    alert("Player 1: You win!");
-                }
-        }
-
-        if (tempArray2.length >= 3) {
-            for (let i = 0; i < 8; i++)
-                if (tempArray2.match(win[i])) {
-                    counter = 9;
-                    joueur2Win = true;
-                    endGameEvaluation(counter);
-                    alert("Player 2: You win!");
-                }
         }
     })
 });
